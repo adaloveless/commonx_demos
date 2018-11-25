9980be24fed78f29ef548acb0f66adf600d02289
Jason Nelson <jnelson@zivix.net>
2018-11-25 01:51:47 -0600

    delete old bats

--- PUSH_SUBMODULE_CHANGES.bat
+++
@@ -1,6 +0,0 @@
-git push
-cd ..
-git add commonx_demos
-git commit -m "submodule tracking hash change"
-git push
-
--- UPDATE_AND_MAKE_EDITABLE.bat
+++
@@ -1,5 +0,0 @@
-git pull
-git submodule update -remote
-git pull
-git checkout master
-
